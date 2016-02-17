948167e0863e2596b4ae7dabfb1cb639e0af0476
oimosan <zushitomomi@oimo-MBP.local>
2016-02-17 20:39:02 +0900

    add view

---
+++ yowbow/HomeTopViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeTopViewController.h
+//  yowbow
+//
+//  Created by oimosan on 2016/02/17.
+//  Copyright © 2016年 FBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeTopViewController : UIViewController
+
+@end
---
+++ yowbow/HomeTopViewController.m
@@ -0,0 +1,37 @@
+//
+//  HomeTopViewController.m
+//  yowbow
+//
+//  Created by oimosan on 2016/02/17.
+//  Copyright © 2016年 FBC. All rights reserved.
+//
+
+#import "HomeTopViewController.h"
+
+@interface HomeTopViewController ()
+
+@end
+
+@implementation HomeTopViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
