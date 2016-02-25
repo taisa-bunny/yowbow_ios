2a5a5b143ee6194698063667e04a74e4ed136c7d
oimosan <zushitomomi@FL1-118-109-66-129.tky.mesh.ad.jp>
2016-02-26 01:07:13 +0900

    Delegate にコメント追加

--- yowbow/AppDelegate.m
+++ yowbow/AppDelegate.m
@@ -18,14 +18,56 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    // 背景色の設定
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
-
-        HomeTopViewController *homeTopViewController = [[HomeTopViewController alloc] initWithNibName:@"HomeTopViewController" bundle:nil];
-        self.window.rootViewController = homeTopViewController;
+    // ホーム画面のインスタンスを生成
+    HomeTopViewController *homeTopViewController = [[HomeTopViewController alloc] initWithNibName:@"HomeTopViewController" bundle:nil];
+    // ホーム画面に遷移
+    self.window.rootViewController = homeTopViewController;
+    
+/**
+* 初回起動のみチュートリアルを表示 sample
+*/
+//    if ([userDefaults boolForKey:IS_DONE_FIRST_APP_EX] == NO) {
+//        [userDefaults setBool:YES forKey:IS_DONE_FIRST_APP_EX];
+//        [userDefaults synchronize];
+//        [self initFirstView];
+//    }
+//    
+//    // 一番上に
+//    [self.window bringSubviewToFront:exScrollView];
+//    
+//    // ログイン確認
+//    // ここでログインフラグを一旦NOにする
+//    [userDefaults setBool:NO forKey:IS_LOGIN];
+//    [userDefaults synchronize];
+//    if ([userDefaults stringForKey:LOGIN_ID] == nil || [[userDefaults stringForKey:LOGIN_ID] isEqualToString:@""] == YES
+//        || [userDefaults stringForKey:LOGIN_PW] == nil || [[userDefaults stringForKey:LOGIN_PW] isEqualToString:@""] == YES) {
+//        
+//        // 自動ログイン
+//    } else {
+//        [AuthFunction login:[AuthFunction decryptString:[userDefaults stringForKey:LOGIN_ID]]
+//                   password:[AuthFunction decryptString:[userDefaults stringForKey:LOGIN_PW]]
+//                     isAuto:YES
+//                 completion:^(NSInteger statusCode, NSDictionary *responseDictionary) {
+//                     // マイページタブのバッジを更新
+//                     [self setMyBadge:numberFormat(responseDictionary[PARAM_MY_BADGE_COUNT])];
+//                     
+//                     // マイページTOPを更新
+//                     UINavigationController *navigationController = self.tabBarController.viewControllers[4];
+//                     id delegate = navigationController.topViewController;
+//                     if ([delegate respondsToSelector:@selector(didCompleteLogin)]) {
+//                         [delegate didCompleteLogin];
+//                     }
+//                     
+//                 } failure:^(NSString *code) {}];
+//    }
     
     return YES;
 }
+
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
